edc86044854b9f0ad901f33c91bbccdccb3b3cbc
Coder Sourya <thecreativeworldforall@gmail.com>
2023-05-09 22:36:46 +0530

    ITC-1: set up the project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+   
+    <div className="app__container">
+      <span className='app__title'>Income Tax Calculator</span>
+    </div>
+  );
+}
+
+export default App;
