54c8c028d4fc4591d5c9ff6e3a9dd1e4f17f5efc
souryadeepRC <thecreativeworldforall@gmail.com>
2024-11-27 17:30:43 +0530

    Restructure Header UI with middle navigation

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,6 +1,5 @@
-import { NavLink } from "react-router-dom";
+// library
 // icons
-import CurrencyRupeeIcon from "@mui/icons-material/CurrencyRupee";
 // components
 import { Navigation } from "src/components/common/navigation/Navigation";
 // styles
@@ -10,11 +9,9 @@ export const Header = () => {
   // render fns
   return (
     <header className="header__container">
-      <NavLink to="">
-        <CurrencyRupeeIcon className="app-logo" />
-        <span> Tax Calculator </span>
-      </NavLink>
+      <span className="header__text">Tax Calculator</span>
       <Navigation />
+      <div className="header__theme">Theme Button</div>
     </header>
   );
 };
--- src/components/common/navigation/Navigation.tsx
+++ src/components/common/navigation/Navigation.tsx
@@ -1,14 +1,39 @@
+import { ReactNode } from "react";
+// library
+import { Box } from "@mui/material";
 import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 // icons
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import DashboardIcon from "@mui/icons-material/Dashboard";
+import HomeIcon from "@mui/icons-material/Home";
 import SavingsIcon from "@mui/icons-material/Savings";
 // selectors
 import { selectIsMobile } from "src/store/screen/screen-selectors";
 // styles
 import "./Navigation.scss";
-
+interface NavigationOptionProps {
+  icon: ReactNode;
+  label: string;
+  isMobile: boolean;
+}
+const NavigationOption: React.FC<NavigationOptionProps> = ({
+  icon,
+  label,
+  isMobile,
+}) => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      gap={1}
+    >
+      {icon}
+      {!isMobile && <>{label}</>}
+    </Box>
+  );
+};
 export const Navigation = () => {
   // store
   const isMobile: boolean = useSelector(selectIsMobile);
@@ -17,18 +42,39 @@ export const Navigation = () => {
   return (
     <ul className="navigation-list">
       <li>
+        <NavLink to="">
+          <NavigationOption
+            label="Home"
+            isMobile={isMobile}
+            icon={<HomeIcon fontSize="small" />}
+          />
+        </NavLink>
+      </li>
+      <li>
         <NavLink to="income">
-          {isMobile ? <AccountBalanceWalletIcon /> : "Income"}
+          <NavigationOption
+            isMobile={isMobile}
+            label="Income"
+            icon={<AccountBalanceWalletIcon fontSize="small" />}
+          />
         </NavLink>
       </li>
       <li>
         <NavLink to="deduction">
-          {isMobile ? <SavingsIcon /> : "Deduction"}
+          <NavigationOption
+            isMobile={isMobile}
+            label="Deduction"
+            icon={<SavingsIcon fontSize="small" />}
+          />
         </NavLink>
       </li>
       <li>
         <NavLink to="tax-breakup">
-          {isMobile ? <DashboardIcon /> : "Tax Breakup"}
+          <NavigationOption
+            isMobile={isMobile}
+            label="Tax Breakup"
+            icon={<DashboardIcon fontSize="small" />}
+          />
         </NavLink>
       </li>
     </ul>
