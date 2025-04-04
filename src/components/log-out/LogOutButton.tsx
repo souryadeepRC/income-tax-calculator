9692646f6ab8283a58627f4f5feebf9825b438de
SouryadeepRC <thecreativeworldforall@gmail.com>
2025-04-04 17:54:50 +0530

    fix navigation mobile view

--- src/components/log-out/LogOutButton.tsx
+++ src/components/log-out/LogOutButton.tsx
@@ -1,4 +1,5 @@
 // library
+import { IconButton } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import LogoutIcon from "@mui/icons-material/Logout";
 // components
@@ -6,12 +7,13 @@ import { Button } from "src/components/common";
 // store
 import { setLogoutActive } from "src/store/auth/auth-actions";
 import { selectIsMobile } from "src/store/screen/screen-selectors";
-import { IconButton } from "@mui/material";
+// styles
+import classes from "./LogoutButton.module.scss";
 
-interface LogOutButtonProps {
+interface LogoutButtonProps {
   onClick?: () => void;
 }
-const LogOutButton: React.FC<LogOutButtonProps> = ({ onClick }) => {
+const LogoutButton: React.FC<LogoutButtonProps> = ({ onClick }) => {
   const isMobile = useSelector(selectIsMobile);
   const dispatch = useDispatch();
 
@@ -21,14 +23,14 @@ const LogOutButton: React.FC<LogOutButtonProps> = ({ onClick }) => {
   };
   if (isMobile) {
     return (
-      <IconButton className="navigation__logout" onClick={onLogout}>
+      <IconButton className={classes.logout__btn} onClick={onLogout}>
         <LogoutIcon />
       </IconButton>
     );
   }
   return (
     <Button
-      className="navigation__logout"
+      className={classes.logout__btn}
       variant="text"
       border="round"
       startIcon={<LogoutIcon />}
@@ -38,4 +40,4 @@ const LogOutButton: React.FC<LogOutButtonProps> = ({ onClick }) => {
     </Button>
   );
 };
-export default LogOutButton;
+export default LogoutButton;
--- src/components/navigation/NavigationMobile.tsx
+++ src/components/navigation/NavigationMobile.tsx
@@ -1,31 +1,19 @@
-import { useSelector } from "react-redux";
 // library
 import { NavLink } from "react-router";
-import { useToggle } from "triva-ui";
-import { IconButton } from "@mui/material";
-import { Close as CloseIcon, Widgets } from "@mui/icons-material";
 //components
-import LogOutButton from "src/components/log-out/LogOutButton";
-// store
-import { selectUserName } from "src/store/auth/auth-selectors";
+import LogOutButton from "src/components/log-out/LogoutButton";
 // constants
-import { Navigation, NAVIGATION_LIST, NavigationType } from "./Navigation";
+import { NAVIGATION_LIST, NavigationType } from "./Navigation";
 // styles
 import "./Navigation.scss";
 
 const NavigationMobile: React.FC = () => {
-  const userName: string = useSelector(selectUserName);
-  const [isOpen, handleSlider] = useToggle(false);
   return (
     <div className="navigation_mobile__container">
       <ul>
         {NAVIGATION_LIST.map(({ label, Icon, path }: NavigationType) => (
           <li key={label}>
-            <NavLink
-              to={path}
-              data-testid={label}
-              {...(handleSlider ? { onClick: handleSlider } : {})}
-            >
+            <NavLink to={path} data-testid={label}>
               <Icon fontSize="small" />
             </NavLink>
           </li>
